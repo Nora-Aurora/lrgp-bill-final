09af5ef6dc9ac917cda37b0123655afed0f869d2
franloudent-francis <jj1513@srmist.edu.in>
2025-09-30 01:40:44 +0530

    fix th bugs

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -9,7 +9,7 @@ interface ModalProps {
 const Modal: React.FC<ModalProps> = ({ onClose, children }) => {
   return (
     <div 
-      className="fixed inset-0 bg-black bg-opacity-50 z-50 flex justify-center items-center"
+      className="fixed inset-0 z-50 flex justify-center items-center bg-black/30 backdrop-blur-sm"
       aria-labelledby="modal-title"
       role="dialog"
       aria-modal="true"
